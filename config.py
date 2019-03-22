d7ca150ece91a16731ef056d57455ba6f200cbb5
Kazeem Olorisade <b.k.olorisade@bristol.ac.uk>
2019-03-22 10:32:48 +0000

    with .env update

--- config.py
+++ config.py
@@ -11,13 +11,13 @@ class Config(object):
     SERVING_FILE = ''
     CITATIONS_RECORD = ''
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or \
-        'mysql://cirex_admin:p@55w0rd@localhost/cirex_db?charset=utf8mb4&binary_prefix=true'
+        'sqlite:///' + os.path.join(APP_ROOT, 'cirex_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MYSQL_DATABASE_CHARSET = 'utf8mb4'
     ##configs to handle error notification
-    MAIL_SERVER = os.getenv('MAIL_SERVER') or 'smtp.googlemail.com'
-    MAIL_PORT = int(os.getenv('MAIL_PORT'))
-    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME') 
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD') 
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') 
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') 
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') 
     ADMINS = ['qasimbabatunde@gmail.com']
